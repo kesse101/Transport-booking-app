4f327ecf6ae2ca29c9bb104f53774cc4e68b0341
kesse101 <kesse752635@gmail.com>
2026-04-17 18:17:35 +0000

    Add user_roles pivot table migration

---
+++ database/migrations/2026_04_17_000000_create_user_roles_table.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_roles', function ($table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
